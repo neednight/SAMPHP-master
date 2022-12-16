552d9e2ec7de669a75947b0355fc5fdb1cb7067d
Need°night <55444728+neednight@users.noreply.github.com>
2022-12-16 08:32:09 +0100

    unstable

--- server/php/devtest/devgm.php
+++ server/php/devtest/devgm.php
@@ -12,9 +12,5 @@ Event::on('GameModeInit', function() {
 	Server::disableInteriorEnterExits();
 	Server::setWeather(2);
 	
-
-	$classes = require 'classes.php';
-
-	foreach($classes as $class)
-		Server::addPlayerClass($class[0], $class[1], $class[2], $class[3], $class[4], $class[5], $class[6], $class[7], $class[8], $class[9], $class[10]);
+    Server::addPlayerClass(0, 1958.3783, 1343.1572, 15.3746, 269.1425, 0, 0, 0, 0, 0, 0);
 });
