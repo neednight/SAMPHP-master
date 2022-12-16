d782e40705d5e00248c0319da907a7535d8b7597
Need°night <55444728+neednight@users.noreply.github.com>
2022-12-16 08:51:32 +0100

    ustable

--- server/php/devtest/route.php
+++ server/php/devtest/route.php
@@ -1,3 +1,14 @@
 <?php
-    require 'Callbacks/PlayerConnect.php';
+    $classesDir = array (
+        ROOT_DIR.'Callbacks/',
+    );
+    function __autoload($class_name) {
+        global $classesDir;
+        foreach ($classesDir as $directory) {
+            if (file_exists($directory . $class_name . '.php')) {
+                require_once ($directory . $class_name . '.php');
+                return;
+            }
+        }
+    }    
 ?>
