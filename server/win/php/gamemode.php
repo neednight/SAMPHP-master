326d016a42638600de7ebb11262d810c8ee59f14
Need°night <55444728+neednight@users.noreply.github.com>
2022-12-16 12:16:22 +0100

    unstable

--- server/win/php/gamemode.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "core/bootstrap.php";
-
-print("It works!");
-print("Now start creating your own gamemode, without complication!");
-print("Just edit the gamemode.php file in the php folder");
-print("Or create a new folder inside the php directory, create a gamemode.php file");
-print("And change the server.cfg, by adding a line called \"samphpmode <gamemode name>");
