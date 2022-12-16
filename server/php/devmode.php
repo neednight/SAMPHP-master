514cca90b7932fcfea7868a31c7c6b209e1e7a2d
Need°night <55444728+neednight@users.noreply.github.com>
2022-12-16 08:39:22 +0100

    unstable

--- server/php/devmode.php
+++ server/php/devmode.php
@@ -2,7 +2,20 @@
 require 'core/bootstrap.php';
 require 'devtest/route.php';
 
-require 'devtest/devgm.php';
+define('COLOR_WHITE', '0xFFFFFFFF');
+define('COLOR_NORMAL_PLAYER', '0xFF4444FF');
+
+Event::on('GameModeInit', function() {
+	Server::setGameModeText("devGM v0.1 PHP");
+	Server::showPlayerMarkers();
+	Server::showNameTags();
+	Server::setNameTagDrawDistance(40.0);
+	Server::enableStuntBonus(false);
+	Server::disableInteriorEnterExits();
+	Server::setWeather(2);
+	
+    Server::addPlayerClass(0, 1958.3783, 1343.1572, 15.3746, 269.1425, 0, 0, 0, 0, 0, 0);
+});
 
 Command::Register('/memory', function($player) {
 	$player->sendClientMessage(0xFFFFFFFF, "Memory Usage PHP: ".(memory_get_usage(false) / 1024)."KB (".(memory_get_usage(true) / 1024)."KB)");
@@ -15,3 +28,4 @@ Command::register('/garbage', function($player) {
 CMD::register('/rr', function($player) {
 	GameModeExit();
 });
+?>
