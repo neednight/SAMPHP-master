ca62e677b96d0c981af36f27d0523d8191e2b9e6
Need°night <55444728+neednight@users.noreply.github.com>
2022-12-16 08:11:20 +0100

    unstable

---
+++ server/php/devtest/devgm.php
@@ -0,0 +1,30 @@
+<?php
+require 'spawns.php';
+
+define('COLOR_WHITE', '0xFFFFFFFF');
+define('COLOR_NORMAL_PLAYER', '0xFF4444FF');
+
+Event::on('GameModeInit', function() {
+	Server::setGameModeText("devGM v0.1 PHP");
+	Server::showPlayerMarkers();
+	Server::showNameTags();
+	Server::setNameTagDrawDistance(40.0);
+	Server::enableStuntBonus(false);
+	Server::disableInteriorEnterExits();
+	Server::setWeather(2);
+	
+
+	$classes = require 'classes.php';
+
+	foreach($classes as $class)
+		Server::addPlayerClass($class[0], $class[1], $class[2], $class[3], $class[4], $class[5], $class[6], $class[7], $class[8], $class[9], $class[10]);
+});
+
+Event::on('PlayerConnect', function($player) {
+	$player->gameText("~w~Grand Larceny", 3000, 4);
+	$player->sendClientMessage(COLOR_WHITE, "Welcome to Grand Larceny");
+
+	$player->citySelection = -1;
+	$player->hasCitySelected = false;
+	$player->lastCitySelectionTick = GetTickCount();
+});
